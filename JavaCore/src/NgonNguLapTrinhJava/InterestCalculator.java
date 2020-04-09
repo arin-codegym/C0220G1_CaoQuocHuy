a7d58d85b82aa73a71c89a9cabed03cbffe6ac1e
Your Namearin-codegym <caoquochuy21@gmail.com>
2020-04-09 14:49:02 +0700

    new

---
+++ JavaCore/src/NgonNguLapTrinhJava/InterestCalculator.java
@@ -0,0 +1,24 @@
+package NgonNguLapTrinhJava;
+
+import java.util.Scanner;
+
+public class InterestCalculator {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interset_rate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Money: ");
+        money = input.nextDouble();
+        System.out.println("time(month):");
+        month = input.nextInt();
+        System.out.println("Interest rate:");
+        interset_rate = input.nextDouble();
+        double total_interest = 0;
+        for (int i = 0; i < month; i++) {
+            total_interest = money * (interset_rate / 100) / 12 * month;
+        }
+        System.out.println("Total interest :"+total_interest);
+    }
+
+}
