e7b17f4f84c540867b6d28959847fb35da7413a6
arin-codegym <caoquochuy21@gmail.com>
2020-04-14 17:37:39 +0700

    today

--- JavaCore/src/AdvancedObjectOrientedDesign/Comparator/CircleComparatorTest.java
+++ JavaCore/src/AdvancedObjectOrientedDesign/Comparator/CircleComparatorTest.java
@@ -3,6 +3,7 @@ package AdvancedObjectOrientedDesign.Comparator;
 import java.util.Arrays;
 import java.util.Comparator;
 
+
 public class CircleComparatorTest {
     public static void main(String[] args) {
         Circle[] circles = new Circle[3];
@@ -14,7 +15,6 @@ public class CircleComparatorTest {
         for (Circle circle : circles) {
             System.out.println(circle);
         }
-
         Comparator circleComparator = new CircleComparator();
         Arrays.sort(circles, circleComparator);
 
---
+++ JavaCore/src/AdvancedObjectOrientedDesign/Resizeable/Resizeable.java
@@ -0,0 +1,5 @@
+package AdvancedObjectOrientedDesign.Resizeable;
+
+public interface Resizeable {
+    public abstract void resize(double percent);
+}
